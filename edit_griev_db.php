cfa50257cedb61530335fafc986f681fab5237be
Buridi Aditya <buridiaditya@gmail.com>
2018-03-23 10:22:41 +0530

    Adds page transitions and edit profile is integrated

--- edit_griev_db.php
+++ edit_griev_db.php
@@ -5,12 +5,25 @@ if (mysqli_connect_errno())
 {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
 $user_type = $_GET["user_type"];
-$email = $_POST["email"];
-$password = md5($_POST["password"]);
-$query = NULL;
+$id = $_GET['id'];
+$name = $_POST["name"];
+$dob = $_POST["DOB"];
+$age = $_POST["age"];
+$houseno = $_POST["Housenumber"];
+$location = $_POST["Location"];
+$password = $_POST["password"];
+
+/*
 if($user_type == 0){
+
+	if($name != '')
+	if($dob != '')
+	if($age != '')
+	if($houseno != '')
+	if($location != '')
+	if($password != '')
+
 	$query = "SELECT * from End_User eu where eu.email = '$email' and eu.Password = '$password' and exists(SELECT * from Respondent g where g.Adhaar_number = eu.Adhaar_number)";
 	//$query = "SELECT * from End_User eu where eu.Email = '$email' and eu.Password = '$password'"; 
 }
@@ -22,32 +35,7 @@ else if($user_type == 2){
 	$query = "SELECT * from End_User eu where eu.email = '$email' and eu.Password = '$password' and exists(SELECT * from Administrator g where g.Adhaar_number = eu.Adhaar_number)";	
 	//$query = "SELECT * from End_User eu where eu.Email = '$email' and eu.Password = '$password'";
 }
-$result = mysqli_query($con, $query);
-$numResults = mysqli_num_rows($result);
-if($numResults == 0)
-{	
-	if($user_type == 0){
-		header ("Location: signin_with_signup.php?state=1&user_type=$user_type");
-	}
-	elseif($user_type == 1){
-		header ("Location: signin_with_signup.php?state=1&user_type=$user_type");
-	}
-	else{
-		header ("Location: signin.php?state=1&user_type=$user_type");
-	}	
-}
-else{
-	$row = $result->fetch_assoc();
-	$aadhaar = $row["Adhaar_number"];
-	$object = mysqli_fetch_field_direct($result,0);
-	$username = $row["Name"];
-	if($user_type == 0){
-		header ("Location: respondent_homepage.php?aadhaar=$aadhaar"); 	
-	}
-	elseif($user_type == 1){
-		header ("Location: grievant_homepage.php?aadhaar=$aadhaar"); 		
-	}else{		
-		header ("Location: Admin.php?aadhaar=$aadhaar"); 
-	}
-}
+*/
+
+
 ?>
--- respondent_homepage.php
+++ respondent_homepage.php
@@ -1,5 +1,14 @@
 <?php 
-	$name = $_GET['name'];
+	$id = $_GET['aadhaar'];
+	$con = mysqli_connect("127.0.0.1","root","Bsaditya@1998","dbms_demo");
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	$query = "SELECT * from End_User eu where eu.Adhaar_number = '$id'";
+	$result = mysqli_query($con, $query);
+	$row = $result->fetch_assoc();
+	$name = $row["Name"];
 ?>
 <html>
 	<head>
@@ -64,9 +73,9 @@
 		<h3 id = "user" style = "font-family:'Cabin Sketch', serif; font-size: 50px; word-spacing: 0px; text-align:center; color: #15632b;">Welcome <?php echo $name; ?>!</h3>
 		<p id = "user" style = "font-family:'Cabin Sketch', serif; font-size: 50px; word-spacing: 0px; text-align:center; color: #15632b;">Your current rating is  <?php echo $name; ?><p>
 		<div>Complaints with status 0 <button class="button">Take up</button> </div><br/>
-		<a href = respondent_homepage.php style = "font-family: 'Cabin Sketch'; text-align:left ;font-size: 25px; color: #15632b">View my solved Complaints</span>  &nbsp; &nbsp; &nbsp;
-		<a href = respondent_homepage.php style = "font-family: 'Cabin Sketch'; text-align:left ;font-size: 25px; color: #15632b">edit profile</span>  &nbsp; &nbsp; &nbsp;
-		<a href = respondent_homepage.php style = "font-family: 'Cabin Sketch'; text-align:left ;font-size: 25px; color:#15632b">logout</span>
+		<a href = "respondent_homepage.php" style = "font-family: 'Cabin Sketch'; text-align:left ;font-size: 25px; color: #15632b">View my solved Complaints</span>  &nbsp; &nbsp; &nbsp;
+		<a href = "editprofile.php?id=<?php echo $id ?>&user_type=0" style = "font-family: 'Cabin Sketch'; text-align:left ;font-size: 25px; color: #15632b">edit profile</span>  &nbsp; &nbsp; &nbsp;
+		<a href = "signin_with_signup.php?user_type=0&state=0" style = "font-family: 'Cabin Sketch'; text-align:left ;font-size: 25px; color:#15632b">logout</span>
 		
 	</center>
 </html>
